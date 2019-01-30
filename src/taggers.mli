ac7194d4abf1922a152bee53f225340b996f3796
Daniel Perez <daniel@perez.sh>
2019-01-30 21:39:59 +0000

    Fix taggers sig

--- src/taggers.mli
+++ src/taggers.mli
@@ -4,3 +4,4 @@ val all: TracerTypes.Tagger.t List.t
 val tag_output: TracerTypes.Tagger.t
 val tag_storage: TracerTypes.Tagger.t
 val tag_overflow: TracerTypes.Tagger.t
+val tag_used_in_condition: TracerTypes.Tagger.t
