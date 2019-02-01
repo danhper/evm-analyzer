ec56c7fa6e34861f96b22534bb8d100179b0ad77
Daniel Perez <daniel@perez.sh>
2019-02-01 18:20:46 +0000

    Fix build

--- src/traceParser.mli
+++ src/traceParser.mli
@@ -1,5 +1,5 @@
 open Core
 
-val parse_json: Yojson.Safe.json -> Trace.t List.t
+val parse_json: Yojson.Safe.t -> Trace.t List.t
 val parse_file: String.t -> Trace.t List.t
 val parse_string: String.t -> Trace.t List.t
